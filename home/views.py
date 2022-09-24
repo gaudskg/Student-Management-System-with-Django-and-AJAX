3b612c8789557637483da988797dc3455b4fba66
Sachin <gaudsachin43@gmail.com>
2022-09-25 01:51:53 +0530

    adding forms and table

--- home/views.py
+++ home/views.py
@@ -1,7 +1,22 @@
 import imp
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Student
+from django.views.decorators.csrf import csrf_exempt
+
 # Create your views here.
 
 def home(request):
-    return render(request,'index.html')
+    all_entries = Student.objects.all()
+    # print(all_entries)
+    # print("*************************")
+    context = {'data': all_entries}
+    return render(request,'index.html',context)
+
+@csrf_exempt
+def send_data(request):
+    if request.POST():
+        name = request.POST.get('name')
+        print(name,"**********")
+    
+    pass
