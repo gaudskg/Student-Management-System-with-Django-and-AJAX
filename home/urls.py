f4088c62d3ba32bcf6368087cb84a79ce7f91189
Sachin <gaudsachin43@gmail.com>
2022-09-28 01:38:04 +0530

    adding delete functionality

--- home/urls.py
+++ home/urls.py
@@ -2,6 +2,7 @@ from django.urls import path,include
 from . import views
 urlpatterns = [
     path('',views.home, name = 'home'),
-    path('send_data/', views.send_data, name='send_data')
+    path('send_data/', views.send_data, name='send_data'),
+    path('delete_data/', views.delete_data, name='delete_data')
 
 ]
--- home/views.py
+++ home/views.py
@@ -15,11 +15,25 @@ def home(request):
 
 @csrf_exempt
 def send_data(request):
+    # id = request.POST['id']
+    # print(id,"+****************************")
+    
+
     if request.method == "POST":
+        # if request.POST['id']:
+        #     print("HHEHHEHEHHEHEHEHEHEHHEHE")
         email = request.POST['email']
         name = request.POST['name']
         branch = request.POST['branch']
-        values = Student.objects.create(name=name,email=email,branch=branch)
-        values.save()
+        # values = Student.objects.create(name=name,email=email,branch=branch)
+        # values.save()
         print(f"{name}*******{email}************{branch}")
         return JsonResponse({'status':200})
+
+
+@csrf_exempt
+def delete_data(request):
+    student_id = request.POST['student_id']
+    b = Student.objects.get(id = student_id)
+    b.delete()
+    return JsonResponse({'status':200})
